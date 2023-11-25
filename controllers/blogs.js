17803c4b9de7aef920bba22b16ee7722c60b1656
Katri <katri.kuoppala@helsinki.fi>
2023-11-25 20:53:49 +0200

    4.2 blogilista, step2

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,23 +3,23 @@ const Blog = require('../models/blog')
 
 
 blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  
-  blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(savedBlog => {
-        response.json(savedBlog)
-      })
-      .catch(error => next(error))
-  })
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
 
-  module.exports = blogsRouter
+
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
