c64e5f6756f594fe5984faddad6d7527e67577c3
Katri <katri.kuoppala@helsinki.fi>
2023-11-27 10:26:45 +0200

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+    console.log('likes', likes)
+    const totalLikes = likes.reduce(
+        (accumulator, currentValue) => accumulator + currentValue, 0
+    )
+    console.log('total', totalLikes)
+    return totalLikes
+}
   
-  module.exports = {
-    dummy
-  }
+module.exports = {
+    dummy,
+    totalLikes,
+}
