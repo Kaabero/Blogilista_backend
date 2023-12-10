6b63b5dbb6dcbc1c901208338a9a4ed611617975
Katri <katri.kuoppala@helsinki.fi>
2023-12-10 20:27:30 +0200

    4.19: blogilistan laajennus, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -19,9 +28,14 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   //const user = await User.findById(body.userId)
-  const users = await User.find({})
-  console.log('users', users)
+  //const users = await User.find({})
+  console.log('user', user)
   const likes = body.likes !== undefined ? body.likes : 0
   console.log('bodytitle', body.title)
   
@@ -34,12 +48,12 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: likes,
-    user: users[0]._id 
+    user: user._id 
     
   })
   const savedBlog = await blog.save()
-  users[0].blogs = users[0].blogs.concat(savedBlog._id)
-  await users[0].save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
