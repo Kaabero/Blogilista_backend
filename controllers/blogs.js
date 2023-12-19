1f81a9d185a253d9ea8890ce5d340db1711a760a
Katri <katri.kuoppala@helsinki.fi>
2023-12-19 16:33:22 +0200

    5.8*: blogilistan frontend, step8 ja 5.9: blogilistan frontend, step9

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -77,16 +77,22 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
 
 blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
+  console.log('backendbody', body)
+  console.log('user', body.user)
 
   const blog = {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: body.user.id
+  
   }
+
+  console.log('backendblog', blog)
     
-  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  response.status(200).end()
+  const blogToReturn = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.status(200).json(blogToReturn)
 
 })
 
